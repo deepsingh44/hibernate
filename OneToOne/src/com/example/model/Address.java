10ba801af75f209bf4ac26f8f87cabb5dedfec16
Deep Singh <Deep Singh@DESKTOP-T0PSQD7>
2020-08-20 19:39:25 +0530

    One to One xml based example.

---
+++ OneToOne/src/com/example/model/Address.java
@@ -0,0 +1,32 @@
+package com.example.model;
+
+public class Address {
+	private int id;
+	private String name;
+	private Student student;
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
